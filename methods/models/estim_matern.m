b08c7a329fdd95ccf0526c5ca0a818def0649774
Julien Bect <julien.bect@centralesupelec.fr>
2023-12-20 22:26:40 +0100

    estim_matern.m: Polish

--- methods/models/estim_matern.m
+++ methods/models/estim_matern.m
@@ -1,16 +1,16 @@
 % Estimate Matern parametrs with constraint on regularity
 
-function [cov, param, ind_cov] = estim_matern (xi, zi, prm, opt_cov_list)
+function [cov, param, ind_cov] = estim_matern (xi, zi, prm, cov_idx)
 
 % Default: use all covariance functions from prm.lis_cov
 if nargin < 4
-    opt_cov_list = 1:(size (prm.list_cov, 1));
+    cov_idx = 1:(size (prm.list_cov, 1));
 end
 
 lhood = inf;
 d = size(xi,2);
 
-for j = opt_cov_list
+for j = cov_idx
     cov_temp = convertStringsToChars (prm.list_cov(j));
     model = stk_model(cov_temp, d);
     param_temp = stk_param_estim(model, xi, zi);
