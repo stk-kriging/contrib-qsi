2ea31ecd6fb505bc60037ed8f8e67e3a31f1a822
Julien Bect <julien.bect@centralesupelec.fr>
2023-12-20 19:02:07 +0100

    Optimization QSI_SUR.m and proba_tau_trajs.m

--- methods/criterion/proba_tau_trajs.m
+++ methods/criterion/proba_tau_trajs.m
@@ -1,7 +1,24 @@
 function proba = proba_tau_trajs(bool, alpha)
 
-bool = prod(bool, 2);
-bool = (mean(bool,4) <= alpha);
-proba = mean(bool, 5);
+[config_nVar, prm_M] = size (bool);
+config_keep_x = size (bool{1,1}, 1);
+
+proba = zeros (config_keep_x, config_nVar);
+
+for k = 1:config_nVar
+
+    % Product of indicator functions (intersection)
+    b = bool{k, 1};
+    for m = 2:prm_M
+        b = b & bool{k,m};
+    end
+
+    % Test for each x if the probability is less than alpha
+    b = (mean (b, 2) <= alpha);
+
+    % Average with respect to the trajectories
+    proba(:, k) = mean (b, 3);
+
+end
 
 end
