90559c8b9a545422de7f208acc027ff8098b3408
Romain Ait Abdelmalek-Lomenech <Romain.Ait@outlook.com>
2024-01-03 11:02:44 +0100

    Set config.keep_x = 50 and config.pts_s = 100 for experiments. Fix an error regarding the output arguments of some config files.

--- test_functions/double_camel_config.m
+++ test_functions/double_camel_config.m
@@ -1,16 +1,16 @@
-function [prm, f, s_trnsf] = double_camel_config()
+function config = double_camel_config()
 
 config = struct();
 
 config.pts_init = 40;
 config.pts_x = 1000;
-config.pts_s = 25;
+config.pts_s = 100;
 
 config.nVar = 11;
 config.nTraj = 200;
 config.lognugget = log(10^-6);
 
-config.keep_x = 60;
+config.keep_x = 50;
 config.keep_xs = 200;
 
 config.keep = config.keep_x*config.pts_s;
