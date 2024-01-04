b1edc1364a3c82f01f667fa0d0b52979759344d0
Julien Bect <julien.bect@centralesupelec.fr>
2024-01-04 14:42:05 +0100

    *_config.m: Set pts_init and pts_x using the input dimensions

--- test_functions/hart4_config.m
+++ test_functions/hart4_config.m
@@ -1,12 +1,6 @@
 function config = hart4_config()
 
-config = example_config ();
-
-config.pts_init = 40;
-config.pts_x = 1000;
-
-config.keep = config.keep_x*config.pts_s;
-config.keep2 = config.keep_xs;
+config = example_config (2, 2);
 
 config.T = 100;
 config.axT = 2;
