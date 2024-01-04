1fa17f1436b45ee67e47e97226d036e9d696fea4
Julien Bect <julien.bect@centralesupelec.fr>
2024-01-04 14:17:44 +0100

    *_config.m: Use default values from example_config.m whenever possible

--- test_functions/double_camel_config.m
+++ test_functions/double_camel_config.m
@@ -1,22 +1,12 @@
 function config = double_camel_config()
 
-config = struct();
+config = example_config ();
 
 config.pts_init = 40;
 config.pts_x = 1000;
-config.pts_s = 100;
-
-config.nVar = 11;
-config.nTraj = 200;
-config.lognugget = log(10^-6);
-
-config.keep_x = 50;
-config.keep_xs = 200;
 
 config.keep = config.keep_x*config.pts_s;
 config.keep2 = config.keep_xs;
-
-config.critName = "m";
 
 config.T = 300;
 config.axT = 5;
--- test_functions/example_config.m
+++ test_functions/example_config.m
@@ -7,10 +7,11 @@ config.pts_init = 20; %number of points in the initial design
 config.pts_x = 500; %number of points sampled in X
 config.pts_s = 100; %number of points sampled in S
 
-config.nVar = 20; %number of points of the Gaussian quadrature used to evaluate criteria
-config.nTraj = 250; %number of trajectories for the evaluation of the QSI criteria (QSI only)
+config.nVar = 11; %number of points of the Gaussian quadrature used to evaluate criteria
+config.nTraj = 200; %number of trajectories for the evaluation of the QSI criteria (QSI only)
+config.lognugget = log(10^-6); %log-variance of the nugget effect
 
-config.keep_x = 15; %number of points to keep in X (QSI only)
+config.keep_x = 50; %number of points to keep in X (QSI only)
 config.keep_xs = 200; %number of candidate points to keep (QSI only)
 
 config.keep = config.keep_x*config.pts_s; %number of points to keep in X x S (joint SUR only)
