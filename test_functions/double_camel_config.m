1a34aafd338f8e322b1ad8d565463fad73e42b74
Julien Bect <julien.bect@centralesupelec.fr>
2024-01-04 15:01:17 +0100

    *_config.m: Polish comments

--- test_functions/double_camel_config.m
+++ test_functions/double_camel_config.m
@@ -1,8 +1,12 @@
-function config = double_camel_config()
+function config = double_camel_config ()
 
+% Set default parameters
 config = example_config (2, 2);
 
+% Number of iterations of the algorithm
 config.T = 300;
+
+% Number of iterations between two performance evaluations
 config.axT = 5;
 
 end
