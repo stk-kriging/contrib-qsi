7f96108382d600b4e828f107dc608f2874b7a08c
Julien Bect <julien.bect@centralesupelec.fr>
2024-01-04 14:17:44 +0100

    *_config.m: Use default values from example_config.m whenever possible

--- test_functions/branin_mod_config.m
+++ test_functions/branin_mod_config.m
@@ -1,22 +1,12 @@
 function config = branin_mod_config()
 
-config = struct();
+config = example_config ();
 
 config.pts_init = 20;
 config.pts_x = 500;
-config.pts_s = 100;
-
-config.nVar = 11;
-config.nTraj = 200;
-config.lognugget = log(10^-6);
-
-config.keep_x = 50;
-config.keep_xs = 200;
 
 config.keep = config.keep_x*config.pts_s;
 config.keep2 = config.keep_xs;
-
-config.critName = "m";
 
 config.T = 30;
 config.axT = 1;
