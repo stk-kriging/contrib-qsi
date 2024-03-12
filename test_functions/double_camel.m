742a51d12556b95b25edd98c33e5f6eceafa87db
Romain Ait Abdelmalek-Lomenech <Romain.Ait@outlook.com>
2024-03-12 17:30:47 +0100

    Fix missing dependancy in double_camel.m

--- test_functions/double_camel.m
+++ test_functions/double_camel.m
@@ -1,8 +1,30 @@
 function y = double_camel(x)
 
-x1 = double(x(:,[1,3]));
-x2 = double(x(:,[2,4]));
+%first term
+xx = double(x(:,[1,3]));
 
-y = (1/2)*(funct.camel_back(x1) + funct.camel_back(x2));
+x1 = xx(:,1);
+x2 = xx(:,2);
+
+term1 = (4-2.1*x1.^2+(x1.^4)/3) .* x1.^2;
+term2 = x1.*x2;
+term3 = (-4+4*x2.^2) .* x2.^2;
+
+y1 = term1 + term2 + term3;
+
+%second term
+xx = double(x(:,[2,4]));
+
+x1 = xx(:,1);
+x2 = xx(:,2);
+
+term1 = (4-2.1*x1.^2+(x1.^4)/3) .* x1.^2;
+term2 = x1.*x2;
+term3 = (-4+4*x2.^2) .* x2.^2;
+
+y2 = term1 + term2 + term3;
+
+
+y = (y1 + y2)/2;
 
 end
