492d998238afa4f35b6d09755f0b2074210c6783
Romain Ait Abdelmalek-Lomenech <romain.ait@centralesupelec.fr>
2024-07-10 15:35:43 +0200

    Updating example_struct.m and example_config.m

--- test_functions/example_struct.m
+++ test_functions/example_struct.m
@@ -1,3 +1,6 @@
+% This file is used to describes the meaning of all the fields involved
+% in a funct_struct.m file.
+
 % Copyright Notice
 %
 % Copyright (C) 2024 CentraleSupelec
@@ -7,27 +10,29 @@
 
 function [prm, f, s_trnsf] = example_struct()
 
-f = @fnct; %function of interest
+f = @branin_mod; % function of interest
 
-s_trnsf = @function_s_trnsf; %inverse transformation of P_s
+s_trnsf = @branin_mod_s_trnsf; %inverse transformation of P_s
 
 prm = struct();
 
-prm.M = 1; %dim of the outputs
+prm.M = 1; % dim of the outputs
 
-prm.dim_x = 1; %dim of X
-prm.dim_s = 1;%dim of S
+prm.dim_x = 1; % dim of X
+prm.dim_s = 1; % dim of S
 
-prm.BOX = stk_hrect([[0;10],[0;15]]); %stk objects for X, S and XxS
+% STK BOX objects for X, S and XxS
+prm.BOX = stk_hrect([[0;10],[0;15]]); 
 prm.BOXx = stk_hrect(repmat([0;10],1,prm.dim_x));
 prm.BOXs = stk_hrect(repmat([0;15],1,prm.dim_s));
 
-prm.const = [-inf ; 7.5]; %Critical region of the form C = [const(1, 1) ; const(2,1)] x ... x [const(1, prm.M) ; const(2,prm.M)]
-prm.alpha = 0.05; %threshold \alpha
+prm.const = [-inf ; 7.5]; % Critical region of the form C = [const(1, 1) ; const(2,1)] x ... x [const(1, prm.M) ; const(2,prm.M)]
+prm.alpha = 0.05; %threshold alpha
 
-prm.name = "branin_mod"; %name of the function for saving results
-prm.name_sub = ["branin_mod"]; %name of the sub-functions
+prm.name = "branin_mod"; % name of the function for saving results
+prm.name_sub = ["branin_mod"]; % name of the sub-functions (useful if prm.M > 1)
 
-prm.list_cov = ["stk_expcov_aniso"; "stk_materncov32_aniso"; "stk_materncov52_aniso"; "stk_gausscov_aniso"]; %list of possible covariances models.
+% list of possible covariances models, implemented in STK
+prm.list_cov = ["stk_expcov_aniso"; "stk_materncov32_aniso"; "stk_materncov52_aniso"; "stk_gausscov_aniso"];
 
 end
